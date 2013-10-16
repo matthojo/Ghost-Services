c32547e59408ed61f44fe8c156c79e9115b7c92b
Matthew Harrison-Jones <matthojo@gmail.com>
2013-10-16 15:38:31 +0100

    Added traffic light guide

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     var dataSource = 'services.json';
-    var source   = "{{#each sites}}<tr><td>{{title}}</td><td><a href='{{URL}}'>{{URL}}</a></td><td><i title='Support level' class='support-level-{{support-level}}'></i></td><td class='center'>{{#if setup}}<a href='{{setup}}'>YES</a>{{/if}}</td><td>{{#if verified}}<i title='Verified by Ghost' class='verified'></i>{{/if}}</td></tr>{{/each}}";
+    var source   = "{{#each sites}}<tr><td>{{title}}</td><td><a href='{{URL}}'>{{URL}}</a></td><td class='center'><i title='Support level' class='support-level-{{support-level}}'></i></td><td class='center'>{{#if setup}}<a href='{{setup}}'>YES</a>{{/if}}</td><td class='center'>{{#if verified}}<i title='Verified by Ghost' class='verified'></i>{{/if}}</td></tr>{{/each}}";
 	var template = Handlebars.compile(source);
     $.getJSON(dataSource, function (data) {
 		var html    = template(data);
