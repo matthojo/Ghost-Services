606825c113d750291dd64de448caece2008cf5e2
Matthew Harrison-Jones <matthojo@gmail.com>
2013-10-16 12:42:51 +0100

    Renamed .json file and improved stlying

--- _site/README.md
+++ _site/README.md
@@ -2,3 +2,5 @@ Ghost Services
 ===============
 
 This is a repository of services that [Ghost](http://ghost.org) works on. Any additional install instruction will be specified. If the service has a 'Verfied by Ghost' icon, it has be checked by official Ghost contributers that it all works.
+
+If you would like to add your hosting or cloud service to the list, please Fork the repository add in your service to the `services.json` file and submit a Pull Request.
--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    var dataSource = 'sites.json';
-    var source   = "{{#each sites}}<tr><td>{{title}}</td><td><a href='{{URL}}'>{{URL}}</a></td><td><a href='{{setup}}'>YES</a></td><td>{{#if verified}}<i title='Verified by Ghost' class='verified'></i>{{/if}}</td></tr>{{/each}}";
+    var dataSource = 'services.json';
+    var source   = "{{#each sites}}<tr><td>{{title}}</td><td><a href='{{URL}}'>{{URL}}</a></td><td class='center'><a href='{{setup}}'>YES</a></td><td>{{#if verified}}<i title='Verified by Ghost' class='verified'></i>{{/if}}</td></tr>{{/each}}";
 	var template = Handlebars.compile(source);
     $.getJSON(dataSource, function (data) {
 		var html    = template(data);
