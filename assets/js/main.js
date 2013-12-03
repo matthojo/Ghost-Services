57410d6ba6dbd0472618382d357a47acba969dae
Matthew Harrison-Jones <matthojo@gmail.com>
2013-12-03 16:21:12 +0000

    Move the content away from the `master` branch and redirect to `gh-pages`.

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    var dataSource = 'services.json';
-    var source   = "{{#each sites}}<tr><td>{{title}}</td><td><a href='{{URL}}'>{{URL}}</a></td><td class='center'><i title='Support level' class='support-level-{{support-level}}'></i></td><td class='center'>{{#if one-click}}<i title='One-click install' class='support-level-full'></i>{{/if}}</td><td class='center'>{{#if setup}}<a href='{{setup}}'>YES</a>{{/if}}</td><td class='center'>{{#if verified}}<i title='Verified by Ghost' class='verified'></i>{{/if}}</td></tr>{{/each}}";
-	var template = Handlebars.compile(source);
-    $.getJSON(dataSource, function (data) {
-		var html    = template(data);
-		$(".sites tbody").html(html);
-    });
- });
